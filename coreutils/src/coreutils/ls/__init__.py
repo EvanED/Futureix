a0e3febc833a1dcd41b8e48ec6b1e8a9ceaba30a
Evan Driscoll <driscoll@cs.wisc.edu>
2012-01-07 23:50:32 -0600

    Comment and add some blank files

--- coreutils/src/coreutils/ls/__init__.py
+++ coreutils/src/coreutils/ls/__init__.py
@@ -1,1 +1,10 @@
-print "hello"
+def ls(directory, pattern, recursive):
+    """Emits JSON entries for files matching the criteria.
+    
+    directory - the name of the directory to search
+    pattern   - a globbing pattern to search for
+    recursive - boolean indicating whether to do a
+                recursive search
+    """
+    
+    pass

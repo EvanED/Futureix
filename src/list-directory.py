cee868953db9e58cc628a430ffa4e6287f9dae17
Evan Driscoll <driscoll@cs.wisc.edu>
2012-07-05 21:34:09 -0500

    Output information for files listed separately on the command line

--- src/list-directory.py
+++ src/list-directory.py
@@ -2,8 +2,23 @@
 
 import posix2  #FIXME after moving readdir to generic
 pyreaddir = posix2
+import os
 import sys
 import json
+
+def output_for_path(path):
+    # If it's a file, we just list it. If it's a directory, we have to
+    # print out the contents.
+    if os.path.isdir(path):
+        dircontents = pyreaddir.readdir(path)
+        for entry in dircontents:
+            print entry.to_json()
+    elif os.path.lexists(path):
+        print path
+    else:
+        msg = "%s: cannot access %s: No such file or directory"
+        sys.stderr.write(msg % (sys.argv[0], path))
+
 
 def main(argv):
     paths = argv[1:]
@@ -11,10 +26,7 @@ def main(argv):
         paths = ["."]
 
     for path in paths:
-        dircontents = pyreaddir.readdir(path)
-        for entry in dircontents:
-            print entry.to_json()
-
+        output_for_path(path)
 
 if __name__ == "__main__":
     main(sys.argv)

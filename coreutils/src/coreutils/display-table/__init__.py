ded0222c9c1a6fbc95963bc3f2d0c6caecf3cc9c
Evan Driscoll <driscoll@cs.wisc.edu>
2012-07-25 12:05:15 -0500

    display-table now outputs the JSON objects it reads

--- coreutils/src/coreutils/display-table/__init__.py
+++ coreutils/src/coreutils/display-table/__init__.py
@@ -1,9 +1,13 @@
+import sys
+import support.json as json
 import argparse
 
 parser = argparse.ArgumentParser()
 
 def main(args):
-    pass
+    for line in sys.stdin:
+        obj = json.loads(line)
+        print obj
 
 if __name__ == "__main__":
     args = parser.parse_args()

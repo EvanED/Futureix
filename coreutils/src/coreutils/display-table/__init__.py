4b8888e53e69dc1436856daeb982b148375d307a
Evan Driscoll <driscoll@cs.wisc.edu>
2012-07-26 09:52:12 -0500

    Have a basic but working display-table

--- coreutils/src/coreutils/display-table/__init__.py
+++ coreutils/src/coreutils/display-table/__init__.py
@@ -4,10 +4,44 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+def read_json_objects(stream):
+    return (json.loads(line) for line in stream)
+
+def get_column_specs(objects):
+    """A colum spec is a tuple (name, size)"""
+    col_specs = {}
+    for obj in objects:
+        for (k,v) in obj.iteritems():
+            v = str(v)
+            spec = col_specs.get(k, (k, len(k)))
+            if len(v) > spec[1]:
+                spec = (k, len(v))
+            col_specs[k] = spec
+    return list(col_specs.itervalues())
+
+
+def print_object(obj, cols):
+    def col_contribution(name, size):
+        try:
+            field = str(obj[name])
+        except KeyError:
+            field = "--"
+        return ("{: <%s}" % str(size)).format(field)
+    print "  ".join(col_contribution(name, size) for (name, size) in cols)
+
+def print_header(cols):
+    names = {name:name for (name, _) in cols}
+    seps = {name:("-"*size) for (name, size) in cols}
+    print_object(names, cols)
+    print_object(seps, cols)
+
+
 def main(args):
-    for line in sys.stdin:
-        obj = json.loads(line)
-        print obj
+    objs = list(read_json_objects(sys.stdin))
+    cols = get_column_specs(objs)
+    print_header(cols)
+    for obj in objs:
+        print_object(obj, cols)
 
 if __name__ == "__main__":
     args = parser.parse_args()

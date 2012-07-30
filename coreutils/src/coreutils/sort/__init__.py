1e2af67ea9d280502914f9e22b1e659b1c397875
Evan Driscoll <driscoll@cs.wisc.edu>
2012-07-29 21:36:05 -0500

    sort-objects now takes '--key=<key>' and sorts by that key. Only one is supported now

--- coreutils/src/coreutils/sort/__init__.py
+++ coreutils/src/coreutils/sort/__init__.py
@@ -3,12 +3,15 @@ import support.json as json
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-k', '--key', help='Comma-separated list of sort keys (first item in the list is sorted first)')
 
 def read_json_objects(stream):
     return (json.loads(line) for line in stream)
 
 def main(args):
-    objs = list(read_json_objects(sys.stdin))
+    assert args.key
+    objs = sorted(read_json_objects(sys.stdin),
+                  key=lambda o: o[args.key])
     for obj in objs:
         print json.dumps(obj)
 
